0a8815250821ee7f3cd70ddba15f85531cacec22
DESKTOP-1SDMCHM\Margarita <match14@yandex.ru>
2019-05-18 17:06:37 +0300

    Верстка оставшихся страниц

--- public/js/main.js
+++ public/js/main.js
@@ -89,40 +89,81 @@ $(document).ready(function () {
 		}
 	});
 
+	// Раскрывающийся блок с заказами
+	$(".js-order-unwap__item").on('click','.js-order-unwap-head',function(event){
+		event.preventDefault();
+		$(this).parent().toggleClass("opened");
+		if($(this).parent().hasClass("opened")){
+			$(this).parent().children(".js-order-unwap-content").slideDown(400);
+			$(this).find('.js-order-unwap-arr').text('-');
+		}
+		else{
+			$(this).parent().children(".js-order-unwap-content").slideUp(400);
+			$(this).find('.js-order-unwap-arr').text('+');
+		}
+	});
+
+	// Раскрывающийся блок с отзывами
+	$(".js-review").on('click','.js-review-link-open',function(event){
+		event.preventDefault();
+		var $parent = $(this).parents('.js-review');
+		$parent.toggleClass("opened");
+		if($parent.hasClass("opened")){
+			$parent.children(".js-review-answer").slideDown(400);
+			$(this).text('Ответ магазина');
+		}
+		else{
+			$parent.children(".js-review-answer").slideUp(400);
+			$(this).text('Свернуть ответ');
+		}
+	});
+
 	// Включаем фоновое видео в блоке
 	$('.js-video-play').click(function(e){
 		$(this).parent('.js-video-content').addClass('hidden');
 		$(this).parent('.js-video-content').siblings('.js-video').trigger('play');
 	});
 
-	// //---------- Маска для телефона -------------
-	// $.mask.definitions['~'] = "[+-]";
-	// $("#phone").mask("(999) 999-9999");
+	//---------- Добавление кода города для телефона -------------
+	if ($('.js-phone').length) {
+		$(".js-phone").intlTelInput({
+			nationalMode: false,
+			separateDialCode: true,
+			initialCountry: 'ru',
+		});
+	}
 
-	// // Вызов функции подгрузки изображений
-	// loadBigImg();
-	// loadBigBacground();
+	//---------- Маска для телефона -------------
+	$.mask.definitions['~'] = "[+-]";
+	$(".js-phone").mask("(999) 999-99-99");
 
-	// // Вызов функции прижатия футера к низу экрана
-	// footerBind('.js-main','.js-header,.js-footer');
-	// $(window).on('resize',function(){footerBind('.js-main','.js-header,.js-footer')});
+	//---------- Реализация табов -------------
+	if ($('.js-tabs-item').length) {
+		$(".js-tabs-list .js-tabs-item:first").addClass("active")
+
+		$(".js-tabs-content .js-tabs-content-item:first").fadeIn();
+
+		$('.js-tabs-item').click(function(e) {
+			e.preventDefault();
+			$(".js-tabs-content-item").hide();
+			$(".js-tabs-item").removeClass("active");
+			$(this).addClass("active");
+			$('#' + $(this).attr('data-item')).fadeIn();
+		});
+	}
+
+	//---------- Переход по шагам оформления заказа -------------
+	if ($('.js-form-step').length) {
+		$('.js-form-step').submit(function (e) {
+			e.preventDefault();
+			$('.js-order-regist-step').removeClass('active');
+			var numStep = $(this).parents('.js-order-regist-step').data('step') + 1;
+			$('.js-order-regist-step[data-step='+numStep+']').addClass('active');
+
+			if (numStep == 3) {
+				$('.js-order-btn').removeClass('btn--inactive');
+			}
+		});
+	}
+
 });
-
-// // Загрузка больших изображений
-// function loadBigImg() {
-// 	var $imgDefer = $('[data-src]');
-
-// 	$imgDefer.each(function(indx, element){
-// 		var urlImgBig = $(this).attr("data-src");
-// 		$(this).attr("src", urlImgBig);
-// 	});
-// }
-
-// function loadBigBacground() {
-// 	var $imgDefer = $('[data-background]');
-
-// 	$imgDefer.each(function(indx, element){
-// 		var urlBackgroundBig = $(this).attr("data-background");
-// 		$(this).css("background-image", "url("+ urlBackgroundBig +")");
-// 	});
-// }
